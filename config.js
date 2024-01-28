b387c293e80237c5ab1dd574ae14c03dda712b89
bwof24 <134535566+bwof24@users.noreply.github.com>
2024-01-28 21:03:53 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-   botToken: "YOUR_BOT_TOKEN", // Modify your bot TOKEN here
+   botToken: "5940482255:AAFWT1Sh2RPtwylt-_w9Jy2kKa-IUbvdqps", // Modify your bot TOKEN here
    hostURL: "YOUR_URL", // Modify your host URL here
 };
 // for port forewording use "ssh -R 80:localhost:5000 serveo.net"
