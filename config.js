a1a0d7e64801b1b470f57e93c9884be683f934e5
bwof24 <134535566+bwof24@users.noreply.github.com>
2024-02-17 08:51:09 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-   botToken: "5940482255:AAFWT1Sh2RPtwylt-_w9Jy2kKa-IUbvdqps", // Modify your bot TOKEN here
+   botToken: "5940482255:AAHqrbUe0EkJ2FVn10oVuFUKBdCwn66JMpg", // Modify your bot TOKEN here
    hostURL: "https://trackdown-6we9.onrender.com", // Modify your host URL here
 };
 // for port forewording use "ssh -R 80:localhost:5000 serveo.net"
