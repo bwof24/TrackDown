3184ea84e3cff09f2b64b4d4a2e37b8d2bbf7f96
bwof24 <134535566+bwof24@users.noreply.github.com>
2024-05-19 22:32:04 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-   botToken: "5940482255:AAHqrbUe0EkJ2FVn10oVuFUKBdCwn66JMpg", // Modify your bot TOKEN here
-   hostURL: "", // Modify your host URL here
+   botToken: "5940482255:AAGu2skh92kVgxYz0kKf_t7VeMOvum7vgow", // Modify your bot TOKEN here
+   hostURL: "https://clickhere.onrender.com", // Modify your host URL here
 };
 // for port forewording use "ssh -R 80:localhost:5000 serveo.net"
